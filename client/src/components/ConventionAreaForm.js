8fc03f8ee7bbf12ca7479db61892cbe484c71e36
John <jaricaya@gmail.com>
2025-01-26 07:06:14 +0800

    added fetch request for conventionareaform

--- client/src/components/ConventionAreaForm.js
+++ client/src/components/ConventionAreaForm.js
@@ -3,4 +3,29 @@ import React, {useState} from "react";
 function ConventionAreaForm({ updateConventionAreas }) {
     const [locationName, setLocationName] = useState("");
     const [venue, setVenue] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newArea = { location_name: locationName, venue: venue};
+        
+        fetch("/convention_areas", {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(newArea),
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error("Failed to add convention area");
+        })
+        .then((data) => {
+            updatingConventionAreas(data);
+            setLocationName("");
+            setVenue("");
+        })
+        .catch((error) => {
+            console.error("Error submitting form:", error);
+        });
+    };
 }
