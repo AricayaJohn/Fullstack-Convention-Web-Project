ec3500df81159d264fef9868bd2fb43c417215b8
John <jaricaya@gmail.com>
2025-01-26 04:34:32 +0800

    added Homepage

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from "react";
+
+function HomePage() {
+    const [conventionAreas, setConventionAreas] = useState([])
+
+    useEffect(() => {
+        fetch('/convention_areas')
+            .then((response) => response.json())
+            .then((data) => setConventionAreas(data))
+            .catch((err) => console.error('Error fetching data:', err));
+    }, []);
+
+    
+}
