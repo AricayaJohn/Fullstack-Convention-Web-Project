e5bc1897072229eff1fceb2fbb03bed9190b178a
John <jaricaya@gmail.com>
2025-01-26 07:17:42 +0800

    added form route to app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./HomePage";
+import ConventionAreaForm from "./ConventionAreaForm";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <h1> Convention Event Management</h1>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route path="/add-convention-area" component={ConventionAreaForm} />
         </Switch>
       </main>
     </Router>
