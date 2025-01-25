6de6c2ea068bc497038aa333f4941b91cc011828
John <jaricaya@gmail.com>
2025-01-26 01:12:07 +0800

    added convention areas in  seed file

--- server/seed.py
+++ server/seed.py
@@ -9,6 +9,14 @@ def cleardata():
     HostCompany.query.delete()
     db.session.commit()
 
+def add_convention_areas():
+    area1 = ConventionArea(location_name='Los Angeles Convention', venue='Staples Center')
+    area2 = ConventionArea(location_name='San Francisco Convention', venue='Oracle Center')
+
+    db.session.add_all([area1, area2])
+    db.session.commit()
+
+
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
