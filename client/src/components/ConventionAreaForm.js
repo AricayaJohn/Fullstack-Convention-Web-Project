a8162e74197606e1795fbbce2077ef32991af9a0
John <jaricaya@gmail.com>
2025-01-26 07:10:16 +0800

    added render form

--- client/src/components/ConventionAreaForm.js
+++ client/src/components/ConventionAreaForm.js
@@ -28,4 +28,31 @@ function ConventionAreaForm({ updateConventionAreas }) {
             console.error("Error submitting form:", error);
         });
     };
+
+    return (
+        <div>
+            <h1>Add Convention Area</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Location Name:
+                    <input 
+                        type="text"
+                        value={locationName}
+                        onChange={(e) => setLocationName(e.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                    Venue:
+                    <input 
+                        type="text"
+                        value={venue}
+                        onChange={(e) => setVenue(e.target.value)}
+                    />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
 }
