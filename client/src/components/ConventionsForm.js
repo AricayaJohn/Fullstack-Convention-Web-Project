30d3384d93315895153c01e2fb69be3dda0abf00
John <jaricaya@gmail.com>
2025-01-27 07:55:04 +0800

    added submit formt handler

--- client/src/components/ConventionsForm.js
+++ client/src/components/ConventionsForm.js
@@ -6,6 +6,27 @@ function AddConventionForm({ areaId, onAddConvention }) {
     const [hostCompanyId, setHostCompanyId] = useState("");
 
     const handleSubmit = (e) => {
-        
-    }
+        e.preventDefault();
+        fetch('conventions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                convention_name: conventionName,
+                days: parseInt(days),
+                convention_area_id: parseInt(areaId),
+                host_company_id: parseInt(hostCompanyId)
+            }),
+        })
+        .then(response => response.json())
+        .then(newConvention => {
+            onAddConvention(newConvention);
+            setConventionName("");
+            setDays("");
+            setHostCompanyId("")
+        })
+        .catch(error => console.error('Error', error));
+    };
+    
 }
