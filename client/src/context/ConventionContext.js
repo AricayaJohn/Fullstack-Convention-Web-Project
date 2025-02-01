2f8be98613a3efd2a64b0b1306c874088f1a0471
John <jaricaya@gmail.com>
2025-02-01 20:19:31 +0800

    added state for context file

--- client/src/context/ConventionContext.js
+++ client/src/context/ConventionContext.js
@@ -1,2 +1,16 @@
 import React, { createContext, useState, useEffectm useCallback } from "react";
 
+export const ConventionContext = createContext();
+
+export function ConventionProvider({ children }) {
+    const [conventionAreas, setConventionAreas] = useState([])
+    const [conventions, setConventions] = useState([]);
+    const [hosts, setHosts] = useState([]);
+    const [selectedConventionHosts, setSelectedConventionHosts] = useState([])
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch("/convention_areas")
+            .then(response => response.json)
+    })
+}
