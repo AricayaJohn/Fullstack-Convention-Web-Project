9793777f25c4842405cf63b3014d57ce11408773
John <jaricaya@gmail.com>
2025-02-13 15:39:25 +0800

    added hosts in area structure

--- client/src/components/HostsInAreaPage.js
+++ client/src/components/HostsInAreaPage.js
@@ -7,3 +7,22 @@ function HostsInAreaPage() {
     const { HostsByAreaId } = useContext(ConventionContext);
     const [ hosts, setHosts ] = useState([])
 }
+
+    return (
+        <div>
+            <h1>Hosts in Area</h1>
+            {hosts.length > 0 ? (
+                <ul>
+                    {hosts.map((host) => (
+                        <li key={host.id}>
+                            <h2>{host.name}</h2>
+                            <p>{host.industry}</p>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No hosts found in this area.</p>
+            )}
+            <Link to="/">Back to Home</Link>
+        </div>
+    );
