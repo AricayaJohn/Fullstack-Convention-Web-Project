e098f671162f48454d8a47ba8f1df6beb856447c
John <jaricaya@gmail.com>
2025-02-27 03:59:02 +0800

    update to formik

--- client/src/components/ConventionAreaForm.js
+++ client/src/components/ConventionAreaForm.js
@@ -1,42 +1,55 @@
 import React, {useState, useContext} from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { ConventionContext } from "../context/ConventionContext";
 
 function ConventionAreaForm() {
-    const [locationName, setLocationName] = useState("");
-    const [venue, setVenue] = useState("");
-
     const { addConventionArea } = useContext(ConventionContext);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newArea = { location_name: locationName, venue: venue};
-        
-        addConventionArea(newArea);
-        setLocationName("");
-        setVenue("");
-    };
+    const formik = useFormik({
+        initialValues: {
+            locationName: '',
+            venue: '',
+        },
+        validationSchema: Yup.object({
+            locationName: Yup.string().required('Location Name'),
+            venue: Yup.string().required('Venue is required'),
+        }),
+        onSubmit: (values, { resetForm }) => {
+            const newArea = {location_name: values.locationName, venue: values.venue };
+            addConventionArea(newArea);
+            resetForm(); 
+        },
+    });
 
     return (
         <div>
             <h1>Add Convention Area</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Location Name:
-                    <input 
-                        type="text"
-                        value={locationName}
-                        onChange={(e) => setLocationName(e.target.value)}
-                    />
-                </label>
-                <br />
-                <label>
-                    Venue:
-                    <input 
-                        type="text"
-                        value={venue}
-                        onChange={(e) => setVenue(e.target.value)}
-                    />
-                </label>
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor="locationName"> Location Name:</label>
+                <input 
+                    id="locationName"
+                    name="locationName"
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.locationName}
+                />
+                {formik.touched.locationName && formik.errors.locationName ? (
+                    <div> {formik.errors.locationName}</div>
+                ) : null}
+                <label htmlFor="venue"> Venue: </label>
+                <input 
+                    id="venue"
+                    name="venue"
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.venue}
+                />
+                {formik.touched.venue && formik.errors.venue ? (
+                    <div>{formik.errors.venue}</div>
+                ) : null}
                 <br />
                 <button type="submit">Submit</button>
             </form>
